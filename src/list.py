cd89b7368ab6d2ff087a72a237ee13e263db5473
Germán Alvarez <kecnhic@gmail.com>
2025-05-02 20:21:26 +0000

    archivo de lista

---
+++ src/list.py
@@ -0,0 +1,10 @@
+listEmployed = ["German Alvarez", "Desarrollo web", True, "GALVAREZ" , "1000"]
+print (listEmployed)
+
+listReadOnly = ("IVA", 1.19)
+print (listReadOnly)
+
+print (f"El empleado del mes es {listEmployed[0]}")
+print (f"El valor del impuesto {listReadOnly[0]} : {listReadOnly[1]}%")
+
+listSet = {"German Alvarez", "Desarrollo web", True, "GALVAREZ" , "1000"}
---
+++ src/types.py
@@ -0,0 +1,8 @@
+texto = "la suma de 2 + 4 es:"
+a = 2
+b = 4
+print (texto , a + b)
+texto = "otra operacion de 50 / 3 es:"
+c = 50
+print (f"{texto} es: {c/b}")
+print ("50" in texto)
