e7c9839d2a2de8c581f7add1f38c0ccf10c89621
Germán Alvarez <kecnhic@gmail.com>
2025-05-02 20:26:47 +0000

    lista de datos

--- src/list.py
+++ src/list.py
@@ -1,4 +1,4 @@
-listEmployed = ["German Alvarez", "Desarrollo web", True, "GALVAREZ" , "1000"]
+listEmployed = ["German Alvarez", "Desarrollador Web", True, "GALVAREZ" , "1000"]
 print (listEmployed)
 
 listReadOnly = ("IVA", 1.19)
@@ -7,4 +7,14 @@ print (listReadOnly)
 print (f"El empleado del mes es {listEmployed[0]}")
 print (f"El valor del impuesto {listReadOnly[0]} : {listReadOnly[1]}%")
 
-listSet = {"German Alvarez", "Desarrollo web", True, "GALVAREZ" , "1000"}
+listSet = {"German Alvarez", "Desarrollador Web", True, "GALVAREZ" , "1000"}
+print (listSet)
+
+dictonary = {
+    'nombre' : 'German Alvarez',
+    'descripcion' : "Desarrollador Web",
+    'activo' : True,
+    'usuario': 'GAVLAREZ',
+    'salario' : 1000
+}
+print (f"El salario del empleado {dictonary['nombre']} es {dictonary['salario']} USD")
