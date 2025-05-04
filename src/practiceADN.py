7b74c72505727b16e3b55a5d831eb265a51fd5da
Germán Alvarez <kecnhic@gmail.com>
2025-05-04 17:12:01 +0000

    ejercicio practico ADN

--- src/practiceADN.py
+++ src/practiceADN.py
@@ -4,28 +4,18 @@
 #cadena2 = "AAAABBBBDDDDDDRRRADCCD"
 # ADCCD
 
-cadena1 = input("Ingresa cadena 1: ")
-cadena2 = input("Ingresa cadena 2: ")
-indice = 0
+cadena1 = input("Ingresa cadena 1: ").upper()
+cadena2 = input("Ingresa cadena 2: ").upper()
 cadenaMayorTemp = ''
 cadenaMayor = ''
 
-for i in range(len(cadena1))    
-        if cadena1[i] == cadena2[j]:            
-
-
-
-for i in range(len(cadena1)):   
-    for j in range(len(cadena2)):
-        if cadena1[i] == cadena2[j]:            
-            for k in range(i + 1, len(cadena1)):                
-                cadenaMayorTemp += cadena1[k]
-                if cadena2.find(cadenaMayorTemp)
-
-        else:
-            if (len(cadenaMayorTemp) > len(cadenaMayor)):
-                cadenaMayor = cadenaMayorTemp
-                cadenaMayorTemp = ''
-                j = len(cadena2) + 1
+for i in range(len(cadena1)):
+    if  cadena2.find(cadena1[i]) != -1:
+        for j in range(i + 1, len(cadena1) + 1):
+            cadenaMayorTemp = cadena1[i:j]   
+            if  cadena2.find(cadenaMayorTemp) == -1:
+                    cadenaMayorTemp = ''
+            elif len(cadenaMayorTemp) > len(cadenaMayor):
+                    cadenaMayor = cadenaMayorTemp
             
 print(f"Cadena Mayor:{cadenaMayor}")
