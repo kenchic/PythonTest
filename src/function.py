7fb8e408e98dde6ac46ad32938f03fb005885e67
Germán Alvarez <kecnhic@gmail.com>
2025-07-16 04:43:11 +0000

    mejorar método con una función

--- src/function.py
+++ src/function.py
@@ -1,5 +1,18 @@
+def mostrar_datos_cuenta(**kwargs):
+    print("📋 Detalles de la cuenta:")
+    for clave, valor in kwargs.items():
+        print(f"🔹 {clave.capitalize()}: {valor}")
+
+        
 numeros = [1 , 5, 3, 9, 12, 5]
 numero_maximo = max(numeros)
 numero_minimo = min(numeros)
 print (f"numero mas alto: {numero_maximo}")
 print (f"numero mas bajo: {numero_minimo}")
+
+mostrar_datos_cuenta(
+    nombre="Luis García",
+    tipo_cuenta="Ahorros",
+    saldo=15000
+)
+
--- src/practiceADN.py
+++ src/practiceADN.py
@@ -4,18 +4,23 @@
 #cadena2 = "AAAABBBBDDDDDDRRRADCCD"
 # ADCCD
 
+def CadenaMayor(cadena1, cadena2):
+    cadenaMayorTemp = ''
+    cadenaMayor = ''
+
+    for i, c in enumerate(cadena1) :
+        if  cadena2.find(c) != -1:
+            for j in range(i + 1, len(cadena1) + 1):
+                cadenaMayorTemp = cadena1[i:j]   
+                if  cadena2.find(cadenaMayorTemp) == -1:
+                        cadenaMayorTemp = ''
+                elif len(cadenaMayorTemp) > len(cadenaMayor):
+                        cadenaMayor = cadenaMayorTemp
+    
+    return cadenaMayor
+
 cadena1 = input("Ingresa cadena 1: ").upper()
 cadena2 = input("Ingresa cadena 2: ").upper()
-cadenaMayorTemp = ''
-cadenaMayor = ''
 
-for i, c in enumerate(cadena1) :
-    if  cadena2.find(c) != -1:
-        for j in range(i + 1, len(cadena1) + 1):
-            cadenaMayorTemp = cadena1[i:j]   
-            if  cadena2.find(cadenaMayorTemp) == -1:
-                    cadenaMayorTemp = ''
-            elif len(cadenaMayorTemp) > len(cadenaMayor):
-                    cadenaMayor = cadenaMayorTemp
-            
+cadenaMayor =  CadenaMayor(cadena1, cadena2)
 print(f"Cadena Mayor:{cadenaMayor}")
